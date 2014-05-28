c43b7bdfaa5ce85136bb698d25db21f5c898479d
ralf0131 <huxing.zhang@gmail.com>
2014-05-28 22:53:01 +0900

    Modify code to avoid overflow.

--- misc/java/IntegerPalindrome.java
+++ misc/java/IntegerPalindrome.java
@@ -10,17 +10,20 @@ public class IntegerPalindrome {
     System.out.println(isPalindrome(121));
     System.out.println(isPalindrome(2882));
     System.out.println(isPalindrome(2342));
+    System.out.println(isPalindrome(0));
+    System.out.println(isPalindrome(Integer.MAX_VALUE));
+    System.out.println(isPalindrome(Integer.MIN_VALUE));
   } 
   
   // 1221
   public static boolean isPalindrome(int a) {
     if (a < 0) return false; // negative number is not palindrome.
-    int left = 1; // the leftmost digit
+    long left = 1L; // the leftmost digit, using long to prevent overflow
     while (left * 10 < a) left *= 10;
-    int temp = a; // remains the integer we need to check
+    long temp = a; // remains the integer we need to check
     while (temp > 0 && left > 1) {
-      int l = temp / left; // get first digit
-      int r = temp % 10;   // get last  digit
+      long l = temp / left; // get first digit
+      long r = temp % 10;   // get last  digit
       if (l != r) return false;
       temp = temp / 10;    // drop last digit
       temp = temp % (left / 10); // drop first digit
